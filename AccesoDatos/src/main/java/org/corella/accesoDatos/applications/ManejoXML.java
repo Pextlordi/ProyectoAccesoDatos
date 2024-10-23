521dc2a87b615a46583ec0e515f9762a03b8f89a
Petteri Ketola <petteri.simo@gmail.com>
2024-10-23 21:07:14 +0200

    Clase de 23 de octubre del 2024

---
+++ AccesoDatos/src/main/java/org/corella/accesoDatos/applications/ManejoXML.java
@@ -0,0 +1,75 @@
+package org.corella.accesoDatos.applications;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class ManejoXML {
+
+    private static final String INDENT_LEVEL = " ";
+
+    private Document leerXML(String rutaFichero) {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setIgnoringComments(true);
+        dbf.setIgnoringElementContentWhitespace(true);
+        Document documentoXML = null;
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            documentoXML = db.parse(new File(rutaFichero));
+            muestraNodo(documentoXML, System.out, 0);
+        } catch (FileNotFoundException | ParserConfigurationException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return documentoXML;
+    }
+
+    private static void muestraNodo(Node nodo, PrintStream printStream, int nivel) {
+        if (nodo.getNodeType() == Node.TEXT_NODE && nodo.getNodeValue().trim().isEmpty()) {
+            return;
+        }
+        for (int i = 0; i < nivel; i++) {
+            printStream.print(INDENT_LEVEL);
+        }
+        switch(nodo.getNodeType()) {
+            case Node.DOCUMENT_NODE:
+                Document doc = (Document) nodo;
+                printStream.println("Documento DOM, version: " + doc.getXmlVersion() + ", codificacion: " + doc.getXmlEncoding());
+                break;
+            case Node.ELEMENT_NODE:
+                printStream.print("<" + nodo.getNodeName());
+                NamedNodeMap listaAtributos = nodo.getAttributes();
+                for (int i = 0; i < listaAtributos.getLength(); i++) {
+                    Node atributo = listaAtributos.item(i);
+                    printStream.print(" @" + atributo.getNodeName() + " [" + atributo.getNodeValue() + "]");
+                }
+                printStream.println(">");
+                break;
+            case Node.TEXT_NODE:
+                printStream.println(nodo.getNodeName() + " [" + nodo.getNodeValue() + "]");
+                break;
+            default:
+                printStream.println("(nodo de tipo: " + nodo.getNodeType() + ")");
+                break;
+        }
+        NodeList childNodes = nodo.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            muestraNodo(childNodes.item(i), printStream, nivel+1);
+        }
+    }
+
+    public void run() {
+        leerXML("pom.xml");
+    }
+
+
+
+}
