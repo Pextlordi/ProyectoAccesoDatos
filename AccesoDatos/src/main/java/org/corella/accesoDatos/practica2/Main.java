bc512a0788231bb072b695aecfc22ebe4afb302b
Petteri Ketola <petteri.simo@gmail.com>
2024-10-25 13:07:33 +0200

    Clase de 24 de octubre del 2024

--- AccesoDatos/src/main/java/org/corella/accesoDatos/practica2/Main.java
+++ AccesoDatos/src/main/java/org/corella/accesoDatos/practica2/Main.java
@@ -1,9 +1,12 @@
 package org.corella.accesoDatos.practica2;
 
 import org.corella.accesoDatos.applications.FicheroAccesoAleatorio;
+import org.corella.accesoDatos.practica2.utils.IrpfRango;
 
 public class Main {
     public static void main(String[] args) {
-        FicheroAccesoAleatorio lectorEscritorAleatorio = new FicheroAccesoAleatorio();
+        System.out.println(IrpfRango.getIrpf5().getKey() + " " + IrpfRango.getIrpf5().getValue());
+        System.out.println(IrpfRango.getIrpf10().getKey() + " " + IrpfRango.getIrpf10().getValue());
+        System.out.println(IrpfRango.getIrpf15().getKey() + " " + IrpfRango.getIrpf15().getValue());
     }
 }
--- AccesoDatos/src/main/java/org/corella/accesoDatos/practica2/utils/IrpfRango.java
+++ AccesoDatos/src/main/java/org/corella/accesoDatos/practica2/utils/IrpfRango.java
@@ -1,7 +1,23 @@
 package org.corella.accesoDatos.practica2.utils;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class IrpfRango {
-    public static Map<String, Map<Integer, Integer>> Irpf5;
+    private static Map.Entry<Integer, Integer> irpf5 = new AbstractMap.SimpleEntry<>(1, 6);
+    private static Map.Entry<Integer, Integer> irpf10 = new AbstractMap.SimpleEntry<>(irpf5.getValue()+1, 10);
+    private static Map.Entry<Integer, Integer> irpf15 = new AbstractMap.SimpleEntry<>(irpf10.getValue()+1, 15);
+
+    public static Map.Entry<Integer, Integer> getIrpf5() {
+        return irpf5;
+    }
+
+    public static Map.Entry<Integer, Integer> getIrpf10() {
+        return irpf10;
+    }
+
+    public static Map.Entry<Integer, Integer> getIrpf15() {
+        return irpf15;
+    }
 }
