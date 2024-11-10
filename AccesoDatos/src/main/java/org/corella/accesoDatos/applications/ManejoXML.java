7dcfb98d11ae55f5feab0827ca51a0f37b9941e7
Petteri Ketola <petteri.simo@gmail.com>
2024-11-10 01:05:59 +0100

    Primer cambio 10 de Noviembre de 2024

--- AccesoDatos/src/main/java/org/corella/accesoDatos/applications/ManejoXML.java
+++ AccesoDatos/src/main/java/org/corella/accesoDatos/applications/ManejoXML.java
@@ -1,5 +1,6 @@
 package org.corella.accesoDatos.applications;
 
+import jdk.jshell.execution.LocalExecutionControl;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
@@ -12,14 +13,24 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.*;
+import java.sql.SQLOutput;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.TemporalField;
 import java.util.*;
 import java.util.stream.IntStream;
 
 public class ManejoXML {
 
     private static final String INDENT_LEVEL = " ";
+    private static final String amarillo = "\u001B[33m";
+    private static final String cyan = "\u001B[36m";
+    private static final String rojo = "\u001B[31m";
+    private static final String verde = "\u001B[32m";
+    private static final String reset = "\u001B[0m";
 
     private Document leerXMLDTD(String rutaFichero) {
+        System.out.println("Lectura y validación DTD: ");
         //DTD
         DocumentBuilderFactory dbf = ValidacionXML.validarXML();
         dbf.setNamespaceAware(true);
@@ -28,15 +39,17 @@ public class ManejoXML {
             DocumentBuilder db = dbf.newDocumentBuilder();
             db.setErrorHandler(new GestorEventos());
             documentoXML = db.parse(new File(rutaFichero));
-            muestraNodo(documentoXML, System.out, 0);
+            //muestraNodo(documentoXML, System.out, 0);
         } catch (FileNotFoundException | ParserConfigurationException e) {
             System.err.println(e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("Lectura: OK");
         return documentoXML;
     }
     private Document leerXMLXSD(String rutaFichero) {
+        System.out.println("Lectura y validación XSD: ");
         //XSD
         DocumentBuilderFactory dbf = ValidacionXML.validarXML(new File("src/main/resources/reedSchema.xsd"));
         dbf.setNamespaceAware(true);
@@ -45,12 +58,13 @@ public class ManejoXML {
             DocumentBuilder db = dbf.newDocumentBuilder();
             db.setErrorHandler(new GestorEventos());
             documentoXML = db.parse(new File(rutaFichero));
-            muestraNodo(documentoXML, System.out, 0);
+            //muestraNodo(documentoXML, System.out, 0);
         } catch (FileNotFoundException | ParserConfigurationException e) {
             System.err.println(e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("Lectura: OK");
         return documentoXML;
     }
 
@@ -86,60 +100,6 @@ public class ManejoXML {
         for (int i = 0; i < childNodes.getLength(); i++) {
             muestraNodo(childNodes.item(i), printStream, nivel+1);
         }
-    }
-
-    private void escribirXML() {
-        try {
-            //DTD
-            //<clientes>
-            //  <cliente DNI=123456789X>
-            //      <nombre>Alejandro</nombre>
-            //  </cliente>
-            //<clientes>
-            //
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document documento = db.newDocument();
-            documento.setXmlVersion("1.0");
-            //<clientes>
-            Element elementoClientes = documento.createElement("clientes");
-            //XSD
-            //elementoClientes.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
-            //elementoClientes.setAttribute("xsi:schemaLocation", "XSDClientes.xsd");
-            //Elemento raiz
-            documento.appendChild(elementoClientes);
-            //<cliente>
-            Element elementoCliente = documento.createElement("cliente");
-            elementoClientes.appendChild(elementoCliente);
-            elementoCliente.setAttribute("DNI", "123456789X");
-            //<nombre>
-            Element elementoNombre = documento.createElement("nombre");
-            elementoCliente.appendChild(elementoNombre);
-            elementoNombre.appendChild(documento.createTextNode("Alejandro"));
-            DOMSource domSource = new DOMSource(documento);
-            Transformer transformador = TransformerFactory.newInstance().newTransformer();
-            transformador.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-            transformador.setOutputProperty(OutputKeys.METHOD, "xml");
-            transformador.setOutputProperty(OutputKeys.INDENT, "yes");
-            //DTD
-            transformador.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "DTDClientes.dtd");
-            transformador.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-
-            //Salida por pantalla
-            StringWriter sw = new StringWriter();
-            StreamResult sr = new StreamResult(sw);
-            transformador.transform(domSource, sr);
-
-            //Salida a FicheroOutXML.xml
-            FileWriter fw = new FileWriter(new File("src/main/resources/FicheroOutXML.xml"));
-            StreamResult sr2 = new StreamResult(fw);
-            transformador.transform(domSource, sr2);
-            fw.close();
-
-        } catch (ParserConfigurationException | TransformerException | IOException e) {
-            System.err.println(e.getMessage());
-        }
-
     }
 
     class GestorEventos extends DefaultHandler {
@@ -182,19 +142,108 @@ public class ManejoXML {
                 listaUnica.add(listaRepetida.item(i).getChildNodes().item(0).getNodeValue());
             }
         }
-        System.out.println("Instructores: ");
+        System.out.println(rojo + "Instructores: ");
         for (String instructor : listaUnica) {
             System.out.println("- " + instructor);
         }
+        System.out.println(reset);
     }
+
+    private void cursosImpartidos(Document documento) {
+        NodeList listaRepetida = documento.getElementsByTagName("title");
+        HashMap<String, Integer> listaUnica = new HashMap<>();
+        for (int i = 0; i < listaRepetida.getLength(); i++) {
+            Node nodo = listaRepetida.item(i).getChildNodes().item(0);
+            if (nodo != null) {
+                if(listaUnica.containsKey(nodo.getNodeValue())) {
+                    listaUnica.put(nodo.getNodeValue(), listaUnica.get(nodo.getNodeValue()) + 1);
+                } else {
+                    listaUnica.put(nodo.getNodeValue(), 1);
+                }
+            }
+        }
+        System.out.println(verde + "Cursos: ");
+        for (Map.Entry<String, Integer> entrada : listaUnica.entrySet()) {
+            System.out.print("- " + entrada.getKey() + " se ha impartido " + entrada.getValue());
+            if (entrada.getValue() == 1) {
+                System.out.println(" vez");
+            } else {
+                System.out.println(" veces");
+            }
+        }
+        System.out.println(reset);
+    }
+
+    private void maxminDuracion(Document documento) {
+        NodeList listaCursos = documento.getElementsByTagName("course");
+        //Lista de nodos por si hay varios cursos máximo o mínimos
+        List<Node> cursosMax = new ArrayList<>();
+        List<Node> cursosMin = new ArrayList<>();
+        cursosMin.add(listaCursos.item(0));
+        cursosMax.add(listaCursos.item(0));
+        int tiempoMin = extraerTiempoTotal(listaCursos.item(0));
+        int tiempoMax = extraerTiempoTotal(listaCursos.item(0));
+        for (int i = 1; i < listaCursos.getLength(); i++) {
+            NodeList horario = listaCursos.item(i).getChildNodes().item(8).getChildNodes();
+            if (horario.item(0).getChildNodes().item(0) != null || horario.item(1).getChildNodes().item(0) != null) {
+                int tiempoCheck = extraerTiempoTotal(listaCursos.item(i));
+                //Comprobar si es menor que el mínimo o igual que el mínimo
+
+                if (tiempoCheck < tiempoMin) {
+                    cursosMin.clear();
+                    cursosMin.add(listaCursos.item(i));
+                    tiempoMin = extraerTiempoTotal(cursosMin.get(0));
+                } else if (tiempoCheck == tiempoMin) {
+                    cursosMin.add(listaCursos.item(i));
+                }
+                //Comprobar si es mayor que el máximo o igual que el mínimo
+                if (tiempoCheck > tiempoMax) {
+                    cursosMax.clear();
+                    cursosMax.add(listaCursos.item(i));
+                    tiempoMax = extraerTiempoTotal(cursosMax.get(0));
+                } else if (tiempoCheck == tiempoMax) {
+                    cursosMax.add(listaCursos.item(i));
+                }
+            }
+        }
+
+        System.out.println(amarillo + "Cursos más largos (Duración semanal de " + tiempoMax + " minutos):");
+        System.out.println("--------------------------");
+        for (Node curso : cursosMax) {
+            muestraNodo(curso, System.out, 0);
+            System.out.println("--------------------------");
+        }
+        System.out.println(reset);
+
+        System.out.println(cyan + "Cursos más cortos (Duración semanal de " + tiempoMin + " minutos):");
+        System.out.println("//////////////////////////");
+        for (Node curso : cursosMin) {
+            muestraNodo(curso, System.out, 0);
+            System.out.println("//////////////////////////");
+        }
+        System.out.println(reset);
+    }
+
+    private int extraerTiempoTotal (Node nodo) {
+        NodeList horario = nodo.getChildNodes().item(8).getChildNodes();
+        String stringCompienzo = horario.item(0).getChildNodes().item(0).getNodeValue();
+        String stringFinalizacion = horario.item(1).getChildNodes().item(0).getNodeValue();
+        LocalTime horaComienzo = LocalTime.parse(stringCompienzo.substring(0, 5));
+        LocalTime horaFinalizacion = LocalTime.parse(stringFinalizacion);
+        int diasSemanales = nodo.getChildNodes().item(7).getChildNodes().item(0).getNodeValue().split("-").length;
+        return (int)(Duration.between(horaComienzo, horaFinalizacion).toMinutes()) * diasSemanales;
+    }
+
     public void run() {
-        //leerXML("pom.xml");
-        //escribirXML();
-        Document documento = leerXMLDTD("src/main/resources/reed.xml");
-        leerXMLXSD("src/main/resources/reed.xml");
+        leerXMLDTD("src/main/resources/reed.xml");
+        Document documento = leerXMLXSD("src/main/resources/reed.xml");
+        //Guardamos uno de los documentos validados
         contarCursos(documento);
         getInstructores(documento);
+        cursosImpartidos(documento);
+        maxminDuracion(documento);
     }
+
 
 
 
